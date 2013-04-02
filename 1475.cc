d4ee821e6485f5132be1676b1f8cdf1164c832ed
fatelei <fatelei@gmail.com>
2013-04-02 17:02:14 +0800

    wait pass 1475

---
+++ 1475.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+void decode(string packet) {
+    string length = 
+}
+
+int main() {
+    int cases;
+    string packet;
+    while (cases--) {
+        getline(cin, packet);
+        decode(packet);
+    }
+    return 0;
+}
